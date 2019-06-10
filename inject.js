00f971c22d93fa02048bfa5b45ad125ea12e2749
Martijn Dwars <martijn.dwars@oracle.com>
2019-06-10 21:55:39 +0200

    Overlay prev/next btn

--- inject.js
+++ inject.js
@@ -1,66 +1,54 @@
 jQuery.noConflict();
 
-var NEIGHBORHOOD = 8;
-
 jQuery(window).load(function () {
-  var disableReload = function () {
-    for (var i = 1; i < 99999; i++) {
-      window.clearInterval(i);
-    }
+  var i = 0;
 
-    window.setInterval(function () {
-      window.isPageVisible = function () {
-        return false;
-      };
-    }, 100);
+  var prevHandler = function () {
+    gotoHandler(i--);
   };
 
-  var isSuccess = function (row) {
-    var $row = jQuery(row);
-    var $status = jQuery($row.find('td:last'));
-
-    return $status.find('img').attr('tooltip') == 'Success';
+  var nextHandler = function () {
+    gotoHandler(i++);
   };
 
-  var hideSuccess = function () {
-    disableReload();
-
-    var table = jQuery('table.pane.bigtable')
-    var rows = jQuery(table).find('tr')
-
-    var flags = rows.map(function (index, row) {
-      return isSuccess(row);
-    }).toArray();
-
-    // Get the non-successful indices
-    var nonSuccessful = [];
-    for (var i = 0; i < flags.length; i++) {
-      if (!flags[i]) {
-        nonSuccessful.push(i);
-      }
-    }
-
-    // Update the neighborhood
-    var n = 0;
-    for (var i = 0; i < nonSuccessful.length; i++) {
-      var flagIndex = nonSuccessful[i];
-
-      for (var j = flagIndex - NEIGHBORHOOD; j < flagIndex + NEIGHBORHOOD; j++) {
-        flags[j] = false;
-      }
-    }
-
-    rows.each(function (index, row) {
-      if (flags[index]) {
-        jQuery(rows[index]).hide();
-        n++;
-      }
+  var gotoHandler = function () {
+    var redImages = jQuery('img').filter(function (index, image) {
+      return jQuery(image).attr('src').endsWith('red.png');
     });
 
-    alert('Hid '+n+' rows!');
+    if (i < 0) {
+      var image = redImages[redImages.length + i % redImages.length];
+    } else {
+      var image = redImages[i % redImages.length];
+    }
+
+    var $image = jQuery(image);
+    $image.css('border', '1px #000 solid');
+
+    var offset = $image.offset();
+
+    jQuery('html, body').animate({
+      scrollTop: offset.top - 30
+    });
   };
 
-  var $hideSuccessBtn = jQuery('<button>Hide success</button>');
-  $hideSuccessBtn.click(hideSuccess);
-  jQuery('#nodeGraph').prepend($hideSuccessBtn);
+  var $nextBtn = jQuery('<button>Next failed</button>');
+  $nextBtn.click(nextHandler);
+  var $prevBtn = jQuery('<button>Prev failed</button>');
+  $prevBtn.click(prevHandler);
+
+  var $overlay = jQuery('<div style=""></div>');
+  $overlay.css('position', 'fixed');
+  $overlay.css('z-index', '9999');
+  $overlay.css('width', '170px');
+  $overlay.css('height', '40px');
+  $overlay.css('bottom', '10px');
+  $overlay.css('right', '10px');
+  $overlay.css('padding', '10px');
+  $overlay.css('background', '#f0f0f0');
+  $overlay.css('border', '1px #000 solid');
+  $overlay.append($prevBtn);
+  $overlay.append($nextBtn);
+
+  jQuery('body').prepend($overlay);
 });
