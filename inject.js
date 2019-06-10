05d697679daba2f89343d9eba9121d020a8bdbcf
Martijn Dwars <martijn.dwars@oracle.com>
2019-06-10 22:09:17 +0200

    Add current/total failed number

--- inject.js
+++ inject.js
@@ -1,6 +1,10 @@
 jQuery.noConflict();
 
 jQuery(window).load(function () {
+  if (window.location.pathname.indexOf("flowGraphTable") < 0) {
+    return;
+  }
+
   var i = 0;
 
   var prevHandler = function () {
@@ -11,10 +15,14 @@ jQuery(window).load(function () {
     gotoHandler(i++);
   };
 
-  var gotoHandler = function () {
-    var redImages = jQuery('img').filter(function (index, image) {
+  var getRedImages = function () {
+    return jQuery('img').filter(function (index, image) {
       return jQuery(image).attr('src').endsWith('red.png');
     });
+  };
+
+  var gotoHandler = function () {
+    var redImages = getRedImages();
 
     if (i < 0) {
       var image = redImages[redImages.length + i % redImages.length];
@@ -30,17 +38,35 @@ jQuery(window).load(function () {
     jQuery('html, body').animate({
       scrollTop: offset.top - 30
     });
+
+    updateNumbers();
+  };
+
+  var updateNumbers = function () {
+    var redImages = getRedImages();
+
+    if (i < 0) {
+      var index = redImages.length + i % redImages.length;
+    } else {
+      var index = i % redImages.length;
+    }
+
+    $currentSpan.text(index + 1);
+    $totalSpan.text(redImages.length);
   };
 
   var $nextBtn = jQuery('<button>Next failed</button>');
-  $nextBtn.click(nextHandler);
+  var $currentSpan = jQuery('<span></span>');
+  var $slash = jQuery('<span> of </span>');
+  var $totalSpan = jQuery('<span></span>');
   var $prevBtn = jQuery('<button>Prev failed</button>');
+  $nextBtn.click(nextHandler);
   $prevBtn.click(prevHandler);
 
   var $overlay = jQuery('<div style=""></div>');
   $overlay.css('position', 'fixed');
   $overlay.css('z-index', '9999');
-  $overlay.css('width', '170px');
+  $overlay.css('width', '220px');
   $overlay.css('height', '40px');
   $overlay.css('bottom', '10px');
   $overlay.css('right', '10px');
@@ -48,7 +74,11 @@ jQuery(window).load(function () {
   $overlay.css('background', '#f0f0f0');
   $overlay.css('border', '1px #000 solid');
   $overlay.append($prevBtn);
+  $overlay.append($currentSpan);
+  $overlay.append($slash);
+  $overlay.append($totalSpan);
   $overlay.append($nextBtn);
 
   jQuery('body').prepend($overlay);
+  updateNumbers();
 });
